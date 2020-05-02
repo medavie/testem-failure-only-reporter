89a45a5b9f579ec02c7b0e4aa9a173d364852171
HerveSeger <herve.seger@medavie.bluecross.ca>
2020-05-02 07:32:18 -0300

    Display test run counter every 50 occurrences

--- failure-only-reporter.js
+++ failure-only-reporter.js
@@ -1,23 +1,24 @@
 const TapReporter = require('testem/lib/reporters/tap_reporter');
 
 class FailureOnlyReporter extends TapReporter {
-  constructor(...args) {
-    super(...args);
-    this._reportCount = 0;
-  }
+	constructor(...args) {
+		super(...args);
+		this._reportCount = 0;
+		this._increment = 0;
+	}
 
-  display(prefix, result) {
-    this._reportCount++;
+	display(prefix, result) {
+		this._reportCount++;
 
-    if (!result.passed) {
-      super.display(prefix, result);
-    }
+		if (!result.passed) {
+			super.display(prefix, result);
+		}
 
-    if (this._reportCount > 100) {
-      this.out.write('pass count: ' + this.pass + '\n');
-      this._reportCount = 0;
-    }
-  }
+		if (parseInt(this._reportCount / 50) !== this._increment) {
+			this.out.write('tests run: ' + this._reportCount + '\n');
+			this._increment++;
+		}
+	}
 }
 
 module.exports = FailureOnlyReporter;
